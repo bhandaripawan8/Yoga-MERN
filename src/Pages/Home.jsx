0f169f03e0403df1c59ce9845dd1d0d4376349b9
Pawan <pawanb78@gmail.com>
2024-03-16 12:09:45 +0000

    banner page done

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,25 +1,82 @@
-import React from 'react'
+import React from 'react';
+import hero1 from '../assets/images/hero1.png';
+import hero2 from '../assets/images/hero2.png';
+import hero3 from '../assets/images/hero3.png';
+import icons1 from '../assets/images/icon01.png'
 
 const Home = () => {
   return (
     <>
-    <section className='hero__section pt-[60px] 2xl:h-[800px]'>
-        <div className="container">
-          <div className='flex flex-col lg: flex-row gap-[90px] items-center justify-between'>
-              {/* hero content */}
-          </div>
-          <div>
-            <div className='lg:w-[570px]'>
-              <h1 className='text-[36px] leading-[46px] text-headingColor font-[800] md:text-[60px] md:leading-[70px]'>Let's collaborate to grow the peace of mind</h1>
-              <p className='text__para'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit mollitia itaque repudiandae. Quo, nam tenetur! Nulla, ea, magnam nam recusandae minima quia blanditiis fuga, a eius officiis nostrum excepturi ut!</p>
-              <button className='btn'>Request an Appointment</button>
+      <section className='hero__section pt-[60px] 2xl:h-[800px]'>
+      <div className="container flex justify-between gap-4">
+        <div className='flex flex-col lg:flex-row gap-[120px] items-center justify-between'>
+          <div className='lg:w-[570px]'>
+            <h1 className='text-[36px] leading-[46px] text-headingColor font-[800] md:text-[60px] md:leading-[70px]'>Let's collaborate to grow the peace of mind</h1>
+            <p className='text__para'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit mollitia itaque repudiandae. Quo, nam tenetur! Nulla, ea, magnam nam recusandae minima quia blanditiis fuga, a eius officiis nostrum excepturi ut!</p>
+            <button className='btn mb-10'>Request an Appointment</button>
+            {/* Client counter section */}
+            <div className='flex flex-col lg:flex-row items-center gap-5 lg:gap-[30px] mt-5 lg:mt-0'>
+              {/* Year of Experience */}
+              <div>
+                <h2 className='text-[35px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-headingColor'>3+</h2>
+                <span className='w-[100px] h-2 bg-yellowColor rounded-full block mt-[-14px]'></span>
+                <p className='text__para'>Year of Experience</p>
+              </div>
+              {/* Clients */}
+              <div>
+                <h2 className='text-[35px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-headingColor'>20+</h2>
+                <span className='w-[100px] h-2 bg-yellowColor rounded-full block mt-[-14px]'></span>
+                <p className='text__para'>Clients</p>
+              </div>
+              {/* Client Satisfaction */}
+              <div>
+                <h2 className='text-[35px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-headingColor'>100%</h2>
+                <span className='w-[100px] h-2 bg-yellowColor rounded-full block mt-[-14px]'></span>
+                <p className='text__para'>Client Satisfaction</p>
+              </div>
             </div>
           </div>
+
+         <div className='flex flex-wrap gap-4 w-90'>
+          {/* First image */}
+          <div className='w-1/2'>
+            <img src={hero1} alt="" className='w-full h-full object-cover' />
+          </div>
+          {/* Second and third images */}
+          <div className='flex flex-col gap-4'>
+            <img src={hero2} alt="" className='w-full h-60 object-cover'/>
+            <img src={hero3} alt="" className='w-full h-60 object-cover'/>
+          </div>
         </div>
+        </div>
+      </div>
+    </section>
+
+    {/* how it works */}
+    <section>
+      <div className="container">
+        <div className='lg:w-[470px] mx-auto'>
+              <h2 className='heading text-center'>
+              Providing the best yoga services
+              </h2>
+              <p className='text__para text-center'>World class services for everyone, from the certified yoga Instructor</p>
+        </div>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-[30px] mt-[30px] lg:mt-[55px]'>
+            <div className='py-[30px] px-5'>
+              <div className='flex items-center justify-center'>
+                <img src= {icons1} alt="" />
+
+              </div>
+
+            </div>
+        </div>
+      </div>
     </section>
     </>
+    
 
-  )
+
+  );
 }
 
-export default Home
+export default Home;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -46,38 +46,39 @@ const Header = () => {
 const toggleMenu = () => menuRef.current.classList.toggle('show__menu')
 
   return (
-    <header className='header' ref={headerRef}>
-      <div className='container flex items-center justify-between'>
-        {/* logo */}
-        <h2>Logo</h2>
-        {/* Navigation */}
-        <nav className='navigation' ref={menuRef} onClick={toggleMenu}>
-          <ul className='menu flex items-center gap-[2.7rem]'>
-            {navlinks.map((link, index) => (
-              <li key={index}>
-                <NavLink
-                  to={link.path}
-                  activeClassName='text-primaryColor text-[16px] leading-7 font-[600]'
-                  className='text-textColor text-[16px] leading-7 font-[500] hover:text-primaryColor'
-                >
-                  {link.display}
-                </NavLink>
-              </li>
-            ))}
-          </ul>
-          {/* <div className='w-[35px] h-[35px] rounded-full'>
-              <h3 className='w-full rounded-full'>userImage</h3>
-          </div> */}
-        </nav>
-        {/* nav right user image */}
-          <Link to={'/login'}>
-            <button className='bg-primaryColor py-2 px-6 text-white font-[600] h-8 w-25 flex items-center rounded-[40px]'>Login</button>
-          </Link>
-        <span className='md:hidden'>
-          <BiMenu className='w-6 h-6 cursor-pointer' onClick={toggleMenu} />
-        </span>
-      </div>
-    </header>
+<header className='header' ref={headerRef}>
+  <div className='container flex items-center justify-between'>
+    {/* logo */}
+    <h2>Logo</h2>
+    {/* Navigation */}
+    <nav className='navigation' ref={menuRef} onClick={toggleMenu}>
+      <ul className='menu flex items-center gap-[2.7rem]'>
+        {navlinks.map((link, index) => (
+          <li key={index}>
+            <NavLink
+              to={link.path}
+              activeClassName='text-primaryColor text-[16px] leading-7 font-[600]'
+              className='text-textColor text-[16px] leading-7 font-[500] hover:text-primaryColor'
+            >
+              {link.display}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+      {/* <div className='w-[35px] h-[35px] rounded-full'>
+          <h3 className='w-full rounded-full'>userImage</h3>
+      </div> */}
+    </nav>
+    {/* nav right user image */}
+    <Link to={'/login'}>
+      <button className='bg-primaryColor py-2 px-6 text-white font-[600] h-8 w-25 flex items-center rounded-[40px]'>Login</button>
+    </Link>
+    <span className='md:hidden'>
+      <BiMenu className='w-6 h-6 cursor-pointer' onClick={toggleMenu} />
+    </span>
+  </div>
+</header>
+
   );
 };
 
