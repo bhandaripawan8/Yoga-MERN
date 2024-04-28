54c090d0644dda876083a6597bb830c5f6c8f525
Pawan <pawanb78@gmail.com>
2024-04-28 23:58:12 +0100

    background gradient fixed

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,14 @@ import Layout from './layout/Layout';
 const App = () => {
   const styles = {
     bg_custom_gradient: {
-      background: 'linear-gradient(100deg, #D8B5FF, #1EAE98)'
+      background: 'linear-gradient(100deg, #f6ea41   , #f048c6)'
     }
   };
 
   return (
   <div style={styles.bg_custom_gradient}>
   
-  <div className='text-[22px] px-[150px] bg-gradient-to-r from-blue-400 to-grey-400'>
+  <div className='text-[22px] px-[150px]'>
       <Layout/>
 </div>
   </div>
