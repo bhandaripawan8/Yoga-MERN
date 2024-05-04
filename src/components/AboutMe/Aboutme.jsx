491a7526ba2158c244a7b61afa646910cea007be
Pawan <pawanb78@gmail.com>
2024-05-04 23:41:13 +0100

    some css added to about me section

--- src/components/AboutMe/Aboutme.jsx
+++ src/components/AboutMe/Aboutme.jsx
@@ -1,16 +1,18 @@
+// AboutMe.js
 import React from 'react';
-import { aboutMe } from '../../data'; 
+import { aboutMe } from '../../data';
+import './AboutMe.css'; // Import the CSS file
 
 const AboutMe = () => {
   const { ourStory, ourValues } = aboutMe;
 
   return (
-    <div>
+    <div className="about-me-container"> {/* Apply the container class */}
       <h2>Our Story</h2>
-      <p>{ourStory}</p>
+      <p className="about-me-story">{ourStory}</p>
 
       <h2>Our Values</h2>
-      <ul>
+      <ul className="about-me-values"> {/* Apply the values class */}
         {ourValues.map((value, index) => (
           <li key={index}>
             <h3>{value.topic}</h3>
