1a587add0b12a076ac06da4f86775c55d4b1161b
Pawan Bhandari <pawanb78@gmail.com>
2024-05-17 15:17:42 +0100

    hero section redesigned

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
-<header className="header flex justify-between items-center px-5 py-6 text-white bg-purple-500">
-<img src="logo.svg" alt="Yoga Logo" className="h-8" />
-<nav className="flex space-x-10 px-5">
-  <a href="" className="text-black hover:underline">Contact</a>
-  <a href="" className="text-black hover:underline">About Us</a>
-  <a href="" className="text-black hover:underline">Sign Up</a>
-</nav>
-</header>
-  )
-}
+    <div className="w-full bg-purple-500 flex justify-center items-center">
+      <div className="w-[70%]">
+        <header className="flex py-6 text-white bg-purple-500 justify-between items-center">
+          <img src="logo.svg" alt="Yoga Logo" className="h-8" />
+          <nav className="flex space-x-10 px-5">
+            <a href="" className="text-white hover:underline">
+              Contact
+            </a>
+            <a href="" className="text-white hover:underline">
+              About Us
+            </a>
+            <a href="" className="text-white hover:underline">
+              Sign Up
+            </a>
+          </nav>
+        </header>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
--- src/components/Hero/Hero1.jsx
+++ src/components/Hero/Hero1.jsx
@@ -4,9 +4,8 @@ import { Link } from "react-router-dom";
 
 const Hero1 = () => {
   return (
-
-    <div className= "bg-gray-100 flex justify-center">
-      <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48 flex justify-center lg:max-w-[70%] ">
+    <div className="bg-gray-100 flex justify-center">
+      <section className="py-12 md:py-24 lg:py-32 xl:py-48 flex justify-center lg:max-w-[70%] ">
         <div className="container px-4 md:px-6 ">
           <div className="grid gap-6 lg:grid-cols-[1fr_500px] lg:gap-12 xl:grid-cols-[1fr_600px]">
             <img
@@ -18,15 +17,17 @@ const Hero1 = () => {
             />
             <div className="flex flex-col justify-center space-y-4">
               <div className="space-y-10">
-                <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl">
-                  Transform Your Body and Mind with <span className="text-purple-500">Hike & Yoga</span>
+                <h1 className="text-3xl font-bold tracking-tighter leading-loose sm:text-5xl xl:text-6xl">
+                  Transform Your Body and Mind with{" "}
+                  <span className="text-purple-500">Hike & Yoga</span>
                 </h1>
+
                 <p className="max-w-[600px] text-gray-500 md:text-xl dark:text-gray-400">
                   Join our hike and yoga classes and experience the
                   transformative power of mindfulness.
                 </p>
               </div>
-              <div className=" flex flex-col gap-3 min-[400px]:flex-row">
+              <div className="py-[12px] flex flex-col gap-3 min-[400px]:flex-row">
                 <Link
                   className="bg-purple-500 inline-flex h-10 items-center justify-center rounded-md px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-black focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
                   href=""
